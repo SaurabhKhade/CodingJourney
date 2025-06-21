b67f8293faea4e82a69592f5d6b2ff6ea7cb8985
Saurabh Khade <66903901+SaurabhKhade@users.noreply.github.com>
2025-06-21 23:16:28 +0530

    Time: 8 ms (42.38%) | Memory: 10.7 MB (91.83%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> map(26);
+        int mxWindow = 0;
+        int i=0, j=0, sum = 0, mx = 0;
+
+        for (; j<s.size(); j++) {
+            map[s[j]-'A']++;
+
+            sum++;
+            mx = mx > map[s[j]-'A'] ? mx : map[s[j]-'A'];
+
+            if ((sum-mx) > k) {
+                while (isWindowReached(map, k)) {
+                    map[s[i]-'A']--;
+                    i++;
+                }
+
+                sum = 0, mx = 0;
+
+                for (int i:map) {
+                    mx = mx > i ? mx : i;
+                    sum += i;
+                }
+            }
+
+            mxWindow = max(mxWindow, j-i+1);
+        }
+
+        return mxWindow;
+    }
+
+    bool isWindowReached(vector<int> &map, int limit) {
+        int mx = 0;
+        int sum = 0;
+
+        for (int i:map) {
+            mx = mx > i ? mx : i;
+            sum += i;
+        }
+
+        return (sum-mx) > limit;
+    }
+};

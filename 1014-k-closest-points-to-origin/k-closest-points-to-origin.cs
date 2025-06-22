bec97fe16db178982be82a6859cdc83fc56e30e3
Saurabh Khade <66903901+SaurabhKhade@users.noreply.github.com>
2025-06-23 00:05:04 +0530

    Time: 22 ms (94.58%) | Memory: 68.2 MB (27.57%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cs
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cs
@@ -3,8 +3,8 @@ public class Solution {
         var list = new List<int[]>(points);
 
         list.Sort((a, b) => {
-            var dist1 = Math.Sqrt(a[0]*a[0] + a[1]*a[1]);
-            var dist2 = Math.Sqrt(b[0]*b[0] + b[1]*b[1]);
+            var dist1 = a[0]*a[0] + a[1]*a[1];
+            var dist2 = b[0]*b[0] + b[1]*b[1];
 
             return dist1.CompareTo(dist2);
         });

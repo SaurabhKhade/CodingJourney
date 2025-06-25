536f66f04dce040e23ea07b67403e62bc8c7d760
Saurabh Khade <66903901+SaurabhKhade@users.noreply.github.com>
2025-06-26 00:23:19 +0530

    Time: 0 ms (100.00%) | Memory: 172.6 MB (35.25%) - LeetSync

--- 3528-reach-end-of-array-with-max-score/reach-end-of-array-with-max-score.cpp
+++ 3528-reach-end-of-array-with-max-score/reach-end-of-array-with-max-score.cpp
@@ -13,31 +13,6 @@ public:
         }
 
         return score + (long long)(n-i-1)* nums[i];
-        
-        // vector<long long> maxScores(n, 0);
-
-        // maxScores[0] = 0;
-
-        // for (long long i = 0; i<n-1; i++) {
-        //     for (long long j = i+1; j<n; j++) {
-        //         // cout<<i<<" "<<j<<" "<<nums[i]<<" "<<nums[j]<<" "<<maxScores[j]<<endl;
-        //         long long newScore = maxScores[i] + (j-i)*nums[i];
-        //         maxScores[j] = newScore > maxScores[j] ? newScore : maxScores[j];;
-        //         // cout<<maxScores[j]<<endl;
-        //     }
-        // }
-
-        // return maxScores[n-1];
     }
 };
-
-// 1 1 2 3
-// - 1 4 7
-// - - 4 7
-//    |
-// 0  1  2  3  4 
-// 4  4  8  12 16
-
-
-
 

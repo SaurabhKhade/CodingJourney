51c6e7e5dc2dc36acad47324c275f9832424335e
Saurabh Khade <66903901+SaurabhKhade@users.noreply.github.com>
2025-06-26 00:04:34 +0530

    Time: 585 ms (8.17%) | Memory: 260.5 MB (6.45%) - LeetSync

---
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<vector<int>> highestPeak(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n));
+        queue<vector<int>> q;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j]) {
+                    visited[i][j] = true;
+                    q.push({i, j, 0});
+                }
+                grid[i][j] = 0;
+            }
+        }
+
+        while (!q.empty()) {
+            auto front = q.front();
+            q.pop();
+            int x = front[0], y= front[1], height = front[2];
+            grid[x][y] = height;
+
+            if (x > 0 && !visited[x-1][y]) {
+                q.push({x - 1, y, height+1});
+                visited[x - 1][y] = true;
+            }
+
+            if (x < m - 1 && !visited[x + 1][y]) {
+                q.push({x + 1, y, height+1});
+                visited[x + 1][y] = true;
+            }
+
+            if (y > 0 && !visited[x][y - 1]) {
+                q.push({x, y - 1, height+1});
+                visited[x][y - 1] = true;
+            }
+
+            if (y < n - 1 && !visited[x][y + 1]) {
+                q.push({x, y + 1, height+1});
+                visited[x][y + 1] = true;
+            }
+        }
+
+        return grid;
+    }
+};

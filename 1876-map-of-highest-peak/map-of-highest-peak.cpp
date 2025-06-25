4188cf6a92e367b8fa7e6344d59e845cc32d663c
Saurabh Khade <66903901+SaurabhKhade@users.noreply.github.com>
2025-06-26 00:16:00 +0530

    Time: 79 ms (57.59%) | Memory: 111.9 MB (89.44%) - LeetSync

--- 1876-map-of-highest-peak/map-of-highest-peak.cpp
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -1,56 +1,58 @@
 const auto _ = std::cin.tie(nullptr)->sync_with_stdio(false);
 
-#define LC_HACK
-#ifdef LC_HACK
-const auto __ = []() {
-    struct ___ {
-        static void _() { std::ofstream("display_runtime.txt") << 1 << '\n'; }
-    };
-    std::atexit(&___::_);
-    return 0;
-}();
-#endif
+// #define LC_HACK
+// #ifdef LC_HACK
+// const auto __ = []() {
+//     struct ___ {
+//         static void _() { std::ofstream("display_runtime.txt") << 1 << '\n'; }
+//     };
+//     std::atexit(&___::_);
+//     return 0;
+// }();
+// #endif
 
 class Solution {
 public:
     vector<vector<int>> highestPeak(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
         vector<vector<bool>> visited(m, vector<bool>(n));
-        queue<vector<int>> q;
+        queue<pair<int, int>> q;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j]) {
                     visited[i][j] = true;
-                    q.push({i, j, 0});
+                    q.push({i, j});
                 }
                 grid[i][j] = 0;
             }
         }
 
         while (!q.empty()) {
-            auto front = q.front();
+            auto [x, y] = q.front();
             q.pop();
-            int x = front[0], y= front[1], height = front[2];
-            grid[x][y] = height;
 
             if (x > 0 && !visited[x-1][y]) {
-                q.push({x - 1, y, height+1});
+                q.push({x - 1, y});
+                grid[x-1][y] = grid[x][y] + 1;
                 visited[x - 1][y] = true;
             }
 
             if (x < m - 1 && !visited[x + 1][y]) {
-                q.push({x + 1, y, height+1});
+                q.push({x + 1, y});
+                grid[x+1][y] = grid[x][y] + 1;
                 visited[x + 1][y] = true;
             }
 
             if (y > 0 && !visited[x][y - 1]) {
-                q.push({x, y - 1, height+1});
+                q.push({x, y - 1});
+                grid[x][y-1] = grid[x][y] + 1;
                 visited[x][y - 1] = true;
             }
 
             if (y < n - 1 && !visited[x][y + 1]) {
-                q.push({x, y + 1, height+1});
+                q.push({x, y + 1});
+                grid[x][y+1] = grid[x][y] + 1;
                 visited[x][y + 1] = true;
             }
         }

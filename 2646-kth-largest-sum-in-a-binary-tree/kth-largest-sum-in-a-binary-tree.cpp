ca75f0680daefe926a41a237e5b738b2b0c71216
Saurabh Khade <66903901+SaurabhKhade@users.noreply.github.com>
2025-06-26 22:39:18 +0530

    Time: 178 ms (7.39%) | Memory: 297.5 MB (7.49%) - LeetSync

--- 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.cpp
+++ 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     long long kthLargestLevelSum(TreeNode* root, int k) {
-        vector<long long> vec;
+        vector<long long> vec(100001, 0);
         traverse(root, vec, 0);
 
         sort(vec.begin(), vec.end(), greater<long long>());
 
-        if (vec.size() < k) return -1;
+        if (vec[k-1] == 0) return -1;
         return vec[k-1];
     }
 
     void traverse(TreeNode* root, vector<long long> &vec, int level) {
         if (root == NULL) return;
-        if (vec.size() == level) vec.resize(level+1);
-        vec[level] += root->val;
+        // cout<<level<<" ";
+        vec[level] += static_cast<long long>(root->val);
 
         traverse(root->left, vec, level+1);
         traverse(root->right, vec, level+1);

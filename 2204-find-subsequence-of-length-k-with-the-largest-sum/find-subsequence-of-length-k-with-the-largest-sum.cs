8f850e49ee9bf785e1a713185e37c6d8ca848e71
Saurabh Khade <66903901+SaurabhKhade@users.noreply.github.com>
2025-06-28 15:22:13 +0530

    Time: 9 ms (71.15%) | Memory: 48.6 MB (59.62%) - LeetSync

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.cs
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.cs
@@ -8,13 +8,18 @@ public class Solution {
 
         elements.Sort((x, y) => y.Item1.CompareTo(x.Item1));
 
-        elements = elements.Take(k).ToList();
+        List<(int, int)> topK = new List<(int, int)>();
 
-        elements.Sort((x, y) => x.Item2.CompareTo(y.Item2));
+        for (int j = 0; j < k; j++)
+        {
+            topK.Add(elements[j]);
+        }
+
+        topK.Sort((x, y) => x.Item2.CompareTo(y.Item2));
 
         var result = new int[k];
         for (int i=0; i<k; i++) {
-            result[i] = elements[i].Item1;
+            result[i] = topK[i].Item1;
         }
 
         return result;

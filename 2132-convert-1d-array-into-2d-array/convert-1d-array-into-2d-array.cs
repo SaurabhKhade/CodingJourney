52c76987e8b770b183d553c22feccda50322c8da
Saurabh Khade <66903901+SaurabhKhade@users.noreply.github.com>
2025-06-30 01:34:00 +0530

    Time: 3 ms (52.08%) | Memory: 68.4 MB (27.08%) - LeetSync

---
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[][] Construct2DArray(int[] original, int m, int n) {
+        if (original.Length != m*n) return new int[0][];
+
+        var result = new int[m][];
+        for (int i=0; i<m; i++) result[i] = new int[n];
+
+        int k=0;
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                result[i][j] = original[k++];
+            }
+        }
+
+        return result;
+    }
+}

4d74984e2e0db6cdbe05db92334bfaa7c78124fd
Saurabh Khade <66903901+SaurabhKhade@users.noreply.github.com>
2025-06-30 01:34:32 +0530

    Time: 2 ms (100.00%) | Memory: 67.6 MB (66.67%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cs
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cs
@@ -3,10 +3,10 @@ public class Solution {
         if (original.Length != m*n) return new int[0][];
 
         var result = new int[m][];
-        for (int i=0; i<m; i++) result[i] = new int[n];
-
         int k=0;
+        
         for (int i=0; i<m; i++) {
+            result[i] = new int[n];
             for (int j=0; j<n; j++) {
                 result[i][j] = original[k++];
             }

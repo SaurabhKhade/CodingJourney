108ef6775d6e8b9c066e74d3ae561a82a34e85c1
Saurabh Khade <66903901+SaurabhKhade@users.noreply.github.com>
2025-06-30 01:05:07 +0530

    Time: 1 ms (9.15%) | Memory: 10.6 MB (45.90%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         vector<int> vec(nums.size(), -1);
-        return max(helper(nums, 0, vec), helper(nums, 1, vec));
+        return helper(nums, 0, vec);
     }
 
     int helper(vector<int>& nums, int n, vector<int> &vec) {
@@ -10,15 +10,5 @@ public:
         if (vec[n] != -1) return vec[n];
 
         return vec[n] = max(helper(nums, n+2, vec) + nums[n], helper(nums, n+1, vec));
-
-        // if (n <= 0) return nums[0];
-        // if (map.find(n) != map.end()) return map[n];
-
-        // int money = 0;
-        // for (int i = n-2; i>=0; i--) {
-        //     money = max(money, helper(nums, i));
-        // }
-
-        // return map[n] = money + nums[n];
     }
 };

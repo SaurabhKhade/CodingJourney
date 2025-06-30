fd023a4740c0c30f098f06da45da4245a35b14a4
Saurabh Khade <66903901+SaurabhKhade@users.noreply.github.com>
2025-06-30 22:38:19 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (35.53%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cs
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cs
@@ -1,9 +1,9 @@
 public class Solution {
     public bool CheckValidString(string s) {
         int stars = 0, open = 0;
-        foreach (var chr in s) {
-            if (chr == '*') stars++;
-            else if (chr == '(') open++;
+        for (int i = 0; i<s.Length; i++) {
+            if (s[i] == '*') stars++;
+            else if (s[i] == '(') open++;
             else {
                 open--;
                 if (open < 0 && stars == 0) return false;
@@ -31,19 +31,5 @@ public class Solution {
         }
 
         return true;
-
-        // if (open > stars) return false;
-
-        // var curr = open;
-        // stars = 0;
-        // int i = s.Length-1;
-
-        // while (i >= 0 && curr > 0) {
-        //     if (s[i] == '(') curr--;
-        //     else if (s[i] == '*') stars++;
-        //     i--;
-        // }
-
-        // return open <= stars;
     }
 }

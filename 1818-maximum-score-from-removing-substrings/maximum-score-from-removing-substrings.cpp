dcb3d5d51da83e1a132d35a06d20cb1ec42283c1
Saurabh Khade <66903901+SaurabhKhade@users.noreply.github.com>
2025-07-23 21:41:22 +0530

    Time: 67 ms (26.77%) | Memory: 34.6 MB (11.35%) - LeetSync

---
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int maximumGain(string s, int x, int y) {
+        int score = 0;
+
+        if (x > y) {
+            countScore(countScore(s, x, 'a', 'b', score), y, 'b', 'a', score);
+        } else {
+            countScore(countScore(s, y, 'b', 'a', score), x, 'a', 'b', score);
+        }
+
+        return score;
+    }
+
+    string countScore(string s, int x, char prev, char next, int &score){
+        stack<char> st;
+        for (int i=0; i<s.size(); i++) {
+            if (st.empty() || !(s[i] == next && st.top() == prev)) {
+                st.push(s[i]);
+            } else {
+                st.pop();
+                score += x;
+            }
+        }
+
+        string str;
+        while (!st.empty()) {
+            str += st.top();
+            st.pop();
+        }
+
+        reverse(str.begin(), str.end());
+        return str;
+    }
+};

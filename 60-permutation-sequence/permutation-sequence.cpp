2051b6366d9223a2093f2ce2a7d8d44ba288c8ad
Saurabh Khade <66903901+SaurabhKhade@users.noreply.github.com>
2025-07-23 19:34:57 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (97.98%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.cpp
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -13,10 +13,6 @@ public:
             factorials[i-1] = i * factorials[i-2];
         }
 
-        // for(int i=0; i<n; i++) {
-        //     cout<<vec[i]<<" "<<factorials[i]<<endl;
-        // }
-
         string result = "";
         while (vec.size() > 0 && k > 0) {
             int elems = vec.size()-1;
@@ -25,16 +21,11 @@ public:
             int sets = k/factorial;
             int isComplete = k%factorial == 0;
             
-            cout<<sets<<" "<<factorial<<" "<<k<<endl;
             k -= (sets)*factorial;
-            cout<<sets<<" "<<factorial<<" "<<k<<endl;
             result += ('0'+vec[sets-isComplete]);
             vec.erase(vec.begin() + sets-isComplete);
         }
 
-        // for (int i=0; i<vec.size(); i++) {
-        //     result += (vec[i] + '0');
-        // }
         for (int i=vec.size()-1; i>=0; i--) {
             result += (vec[i] + '0');
         }
@@ -42,11 +33,3 @@ public:
         return result;
     }
 };
-
-// 1 1 3
-// 2 1 4
-// 3 1 5
-// 4 2 6
-// 5 2 7
-// 6 2 8
-

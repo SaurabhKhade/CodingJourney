f752108ef3dbfcdbf15bc5ee92e3e89013f3ed43
Saurabh Khade <66903901+SaurabhKhade@users.noreply.github.com>
2025-07-27 15:27:52 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (50.25%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.cpp
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.cpp
@@ -9,16 +9,12 @@ public:
             j++;
         }
 
-        // for (int k=0; k<=i; k++) {
-        //     cout<<nums[k]<<" ";
-        // }
-
         int count = 0;
         for (int k=1; k<i; k++) {
             if (nums[k] > nums[k-1] && nums[k] > nums[k+1]) {
                 count++;
             }
-            if (nums[k] < nums[k-1] && nums[k] < nums[k+1]) {
+            else if (nums[k] < nums[k-1] && nums[k] < nums[k+1]) {
                 count++;
             }
         }

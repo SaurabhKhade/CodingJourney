7c5ec9476f68f6ffd8fb561f7c900031f2cddb5a
Saurabh Khade <66903901+SaurabhKhade@users.noreply.github.com>
2025-08-09 23:02:15 +0530

    Time: 24 ms (7.69%) | Memory: 32.2 MB (7.69%) - LeetSync

---
+++ 826-soup-servings/soup-servings.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    private (int a, int b)[] ops = { (100, 0), (75, 25), (50, 50), (25, 75) };
+
+    double Helper(int a, int b, IDictionary<int, IDictionary<int, double>> dp) {
+        if (a <= 0 && b <= 0) return 0.5;
+        if (a <= 0) return 1;
+        if (b <= 0) return 0;
+
+        if (dp.ContainsKey(a) && dp[a].ContainsKey(b)) return dp[a][b];
+
+        double probability = 0;
+
+        foreach (var op in ops) {
+            probability += Helper(a - op.a, b - op.b, dp) * .25;
+        }
+
+        if (!dp.ContainsKey(a)) {
+            dp[a] = new Dictionary<int, double>();
+        }
+
+        return dp[a][b] = probability;
+    }
+
+    public double SoupServings(int n) {
+        if (n > 1e4) return 1;
+
+        var dict = new Dictionary<int, IDictionary<int, double>>();
+        return Helper(n, n, dict);
+    }
+}

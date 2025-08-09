1c6ba4dd0a0ade91bd9231c28175cefd9bcab4b6
Saurabh Khade <66903901+SaurabhKhade@users.noreply.github.com>
2025-08-09 23:08:26 +0530

    Time: 13 ms (15.38%) | Memory: 53.4 MB (7.69%) - LeetSync

--- 826-soup-servings/soup-servings.cs
+++ 826-soup-servings/soup-servings.cs
@@ -1,12 +1,12 @@
 public class Solution {
     private (int a, int b)[] ops = { (100, 0), (75, 25), (50, 50), (25, 75) };
 
-    double Helper(int a, int b, IDictionary<int, IDictionary<int, double>> dp) {
+    double Helper(int a, int b, double?[,] dp) {
         if (a <= 0 && b <= 0) return 0.5;
         if (a <= 0) return 1;
         if (b <= 0) return 0;
 
-        if (dp.ContainsKey(a) && dp[a].ContainsKey(b)) return dp[a][b];
+        if (dp[a,b].HasValue) return dp[a,b].Value;
 
         double probability = 0;
 
@@ -14,17 +14,14 @@ public class Solution {
             probability += Helper(a - op.a, b - op.b, dp) * .25;
         }
 
-        if (!dp.ContainsKey(a)) {
-            dp[a] = new Dictionary<int, double>();
-        }
-
-        return dp[a][b] = probability;
+        dp[a,b] = probability;
+        return probability;
     }
 
     public double SoupServings(int n) {
         if (n > 4900) return 1;
 
-        var dict = new Dictionary<int, IDictionary<int, double>>();
-        return Helper(n, n, dict);
+        var dp = new double?[n+1, n+1];
+        return Helper(n, n, dp);
     }
 }

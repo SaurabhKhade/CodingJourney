ca49a78276c22cc2f2eddd8bc8047a82a84c978d
Saurabh Khade <66903901+SaurabhKhade@users.noreply.github.com>
2025-08-15 11:47:37 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (52.41%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if (n <= 0) return false;
-        while (n > 1) {
-            if (n%4 != 0) return false;
-            n /= 4;
-        }
+        // if (n <= 0) return false;
+        // while (n > 1) {
+        //     if (n%4 != 0) return false;
+        //     n /= 4;
+        // }
 
-        return true;
+        // return true;
+
+        return (n > 0) && !(n & (n-1)) && (n%3 == 1);
     }
 };

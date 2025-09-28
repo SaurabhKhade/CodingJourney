6eff35de29824f4e925217e6efecb46062153dcc
Saurabh Khade <66903901+SaurabhKhade@users.noreply.github.com>
2025-09-28 22:36:01 +0530

    Time: 16 ms (12.27%) | Memory: 25.6 MB (76.13%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
@@ -4,10 +4,14 @@ public:
         int n = nums.size();
         sort(nums.begin(), nums.end());
         for (int i=n-1; i>1; i--) {
-            for (int j=i-1; j>0; j--) {
-                if (nums[j] + nums[j-1] > nums[i]) {
-                    return nums[i] + nums[j] + nums[j-1];
-                }
+            // for (int j=i-1; j>0; j--) {
+                // if (nums[j] + nums[j-1] > nums[i]) {
+                //     return nums[i] + nums[j] + nums[j-1];
+                // }
+            // }
+
+            if (nums[i-1] + nums[i-2] > nums[i]) {
+                return nums[i] + nums[i-1] + nums[i-2];
             }
         }
 

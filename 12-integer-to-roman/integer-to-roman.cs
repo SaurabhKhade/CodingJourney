7d10437125976badca183e92a44b58d01d621e49
Saurabh Khade <66903901+SaurabhKhade@users.noreply.github.com>
2025-10-10 09:54:32 +0530

    Time: 2 ms (87.32%) | Memory: 45.6 MB (93.52%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cs
+++ 12-integer-to-roman/integer-to-roman.cs
@@ -1,15 +1,17 @@
 public class Solution {
-    public string IntToRoman(int num) {
-        var digit = new string[] { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
-        var value = new int[] {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private static readonly string[] Symbols = 
+        { "M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I" };
+    private static readonly int[] Values = 
+        {1000,900,500,400,100,90,50,40,10,9,5,4,1};
 
+    public string IntToRoman(int num) {
         int i = 0;
         var roman = new StringBuilder();
 
         while (num > 0) {
-            while (num < value[i]) i++;
-            roman.Append(digit[i]);
-            num -= value[i];
+            while (num < Values[i]) i++;
+            roman.Append(Symbols[i]);
+            num -= Values[i];
         }
 
         return roman.ToString();

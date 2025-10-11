1c043ee51ed35fdd5a66223bf8281924b4abc8b8
Saurabh Khade <66903901+SaurabhKhade@users.noreply.github.com>
2025-10-11 15:55:38 +0530

    Time: 2 ms (80.69%) | Memory: 63.8 MB (94.31%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cs
+++ 41-first-missing-positive/first-missing-positive.cs
@@ -1,11 +1,14 @@
 public class Solution {
     public int FirstMissingPositive(int[] nums) {
+
+        // Eliminate the negative numbers and zeroes, as they aren't useful for us
         for (int i=0; i<nums.Length; i++) {
             if (nums[i] <= 0) {
                 nums[i] = 10000000;
             }
         }
 
+        // if the value of current element lies within array range, change the element at that position to negative, to mark it as visited
         for (int i=0; i<nums.Length; i++) {
             var abs = Math.Abs(nums[i]);
             if (abs <= nums.Length && abs > 0) {
@@ -13,12 +16,15 @@ public class Solution {
             }
         }
 
+        // check the first non negative number
+        // it means that element with that position value is absent
         for (int i=0; i<nums.Length; i++) {
             if (nums[i] > 0) {
                 return i+1;
             }
         }
 
+        // if nothing found, it means array contains numbers from 1 to n
         return nums.Length + 1;
     }
 }
